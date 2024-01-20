f86ad61262613446644c9b084c50571960974de8
Caitlin Estrada <100039736+caitlinestrada23@users.noreply.github.com>
2024-01-20 16:37:32 -0500

    Assignment 1 Part 1

---
+++ src/assn01/Part2.java
@@ -0,0 +1,14 @@
+package assn01;
+
+public class Part2 {
+    public static void main(String[] args){
+        short sh = 32767;
+    }
+
+    public static void method2(){
+    }
+
+    public static void method3(){
+
+    }
+}
---
+++ src/lec/Lec2.java
@@ -0,0 +1,13 @@
+package lec;
+
+public class Lec2 {
+
+    public static void main (String[] args){
+        int n = 0;
+        m1();
+    }
+
+    public static void m1 (){
+        int n1 = 1;
+    }
+}
